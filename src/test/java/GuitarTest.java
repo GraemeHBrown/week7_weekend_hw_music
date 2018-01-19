4cd58b0c43ec598de279dd2261a581a608650f6e
Graeme Brown <graemehbrown@googlemail.com>
2018-01-19 11:54:13 +0000

    Created IPlayable interface with play method.  Created Guitar class that implements IPlayable.

---
+++ src/test/java/GuitarTest.java
@@ -0,0 +1,49 @@
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class GuitarTest {
+
+    Guitar guitar;
+
+    @Before
+    public void before(){
+        guitar = new Guitar("Martin 000-28", "Mahogany Blocks", "Hardwood", "Chrome tuning machine", 6);
+    }
+
+    @Test
+    public void guitarCanPlay(){
+        assertEquals("Guitar playing", guitar.play());
+    }
+
+
+    @Test
+    public void guitarHasModel(){
+        assertEquals("Martin 000-28", guitar.getModel());
+    }
+
+    @Test
+    public void guitarHasBodyType(){
+        assertEquals("Mahogany Blocks", guitar.getBodyType());
+    }
+
+    @Test
+    public void guitarHasNeckType(){
+        assertEquals("Hardwood", guitar.getNeckType());
+    }
+
+    @Test
+    public void guitarHasHardware(){
+        assertEquals("Chrome tuning machine", guitar.getHardware());
+    }
+
+    @Test
+    public void guitarHasNumberOfStrings(){
+        assertEquals(6, guitar.getNumberOfStrings());
+    }
+
+
+}
